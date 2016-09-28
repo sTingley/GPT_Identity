f48c8138cae285a0017b430d7d8eec89a703f69b
Arun Kumar <arunkumarsekar@hotmail.com>
2016-09-28 17:52:28 +0530

    Add files via upload

--- walletApp-Dev/src/app/Components/CoreIdentity.jsx
+++ walletApp-Dev/src/app/Components/CoreIdentity.jsx
@@ -5,12 +5,89 @@ class UploadIpfsFile extends React.Component {
 
 	constructor(props){
 		super(props);
-		this.state = {};
+		this.state = {
+			docs: {},
+			pubKey: '1dc99871943ad3a715f022273513a393564f9b060c4c047920fc1425b90b7740',
+			selected:'0',
+			files:''
+		};
+		this.inputChangeHandler = this.inputChangeHandler.bind(this);
 	}
 
 	componentDidMount(){
+		$.ajax({
+			url: "http://localhost:5050/ipfs/alldocs/"+this.state.pubKey,
+			dataType: 'json',
+			cache: false,
+			success: function(resp) {
+				this.setState({docs: resp.data.documents});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+		
 		$("#CoreIdentityContainer .modal").modal('show');
         $("#CoreIdentityContainer .modal").on('hidden.bs.modal', this.props.handleHideModal);
+	}
+	
+	uploadHandler(data, additionalParams){
+		var params = {
+			url: "http://localhost:5050/ipfs/upload",
+			type: 'POST',
+			data: data,
+			cache: false,
+			processData: false,
+			contentType: false,
+		};
+		$.extend(params, additionalParams);
+		$.ajax(params);
+	}
+	
+	fileHandler(e){
+		e.preventDefault();
+		if(this.state.selected != "0"){
+			var hash, fileHash;
+			[hash, fileHash] = this.state.selected.split("|");
+			this.props.dataHandler(hash, fileHash);
+		} else {
+			console.log(this.state.files);
+			if(this.state.files.size > 0){
+				var fileInput = $("input[name=newdoc]");
+				var fData = new FormData();
+				fData.append("user_pubkey", this.state.pubKey);
+				 $.each(fileInput[0].files, function(key, value){
+					fData.append(key, value);
+				});
+				var _this = this;
+				var callbacks = {
+					beforeSend: (xhr) => {
+						$("button[name=uploadsubmit]").button('loading');
+						$("button.close").hide();
+					},
+					success: function(resp){
+						if(resp.uploded && resp.uploded.length > 0){
+							var hash = resp.uploded[0].hash,
+								fileHash = resp.uploded[0].file_hash;
+							this.props.dataHandler(hash, fileHash);
+							$("button.close").trigger("click");
+						}
+					}.bind(this),
+					complete: () => {
+						$("button[name=uploadsubmit]").button('reset');
+						$("button.close").show();
+					}
+				};
+				this.uploadHandler(fData,callbacks);
+			}
+		}
+	}
+	
+	inputChangeHandler(e){
+		if(e.target.tagName == "SELECT"){
+			this.setState({selected: e.target.value});
+		} else 
+			this.setState({files: e.target.files[0]});
 	}
 
 	render(){
@@ -29,22 +106,31 @@ class UploadIpfsFile extends React.Component {
                   <form>
                   	<div className="form-group">
                   		<label htmlFor="get-hash">Choose from documents</label>
-                  		<input type="text" className="form-control" placeholder="Type file name" />
+                  		<select className="form-control" onChange={this.inputChangeHandler}>
+							<option value="0">Select Document</option>
+						{(()=>{
+							if(this.state.docs && this.state.docs.length > 0){
+								var i=0;
+								return this.state.docs.map((obj) => {
+									i++;
+									var optsVal = obj.hash+"|"+obj.file_hash;
+									return <option value={optsVal} key={i}>{obj.filename}</option>
+								});
+							} else {
+								return <option value="0">-- Empty --</option>
+							}
+						})()}
+						</select>
                   	</div>
                   	<p style={center}>(or)</p>
                   	<div className="form-group">
-                  		<label htmlFor="uploadfile col-md-12"></label>
-                  		<div className="col-md-9">
-							<input type="file" className="form-control" name="documents"/>
-						</div>
-						<div className="col-md-2">
-							<input type="button" data-loading-text="Uploading Files..." value="Upload" name="upload-file" className="btn btn-primary" />
-						</div>
+                  		<label htmlFor="documents">Upload Document</label>
+                  		<input type="file" className="form-control" name="newdoc" onChange={this.inputChangeHandler}/>
                   	</div>
                   </form>
                 </div>
                 <div className="modal-footer">
-                  <button type="button" className="btn btn-success">Submit</button>
+                  <button type="button" data-loading-text="Processing..." name="uploadsubmit" className="btn btn-success" onClick={this.fileHandler.bind(this)}>Submit</button>
                 </div>
               </div>
             </div>
@@ -57,33 +143,13 @@ class UniqIdForm extends React.Component {
 
 	constructor(props){
 		super(props);
-		this.state = {
-			count: props.min,
-			max: props.max,
-			official_id:[]
-
-		};
-	}
-
-	onFieldChange(){
-
 	}
 
 	render(){
-		var inputAttrs = {
-			addKeys: [13,188],	// Enter and comma
-			inputProps: {
-				placeholder: "use comma(,) to add multiple values",
-				style:{width:'30%'}
-			}
-		};
 		return(
 			<div className="form-group col-md-12">
-				<div className="col-md-4">
+				<div className="col-md-10">
 					<input name="uniq_id_key" className="form-control col-md-4" type="text" placeholder="Label" />
-				</div>
-				<div className="col-md-6">
-					<TagsInput {...inputAttrs} value={this.state.official_id} onChange={(e)=>{ this.onFieldChange("official_id", e) } }  />
 				</div>
 				<div className="col-md-2">
 					<button type="button" onClick={this.props.handleShowModal} className="btn btn-warning pull-right"><span className="glyphicon glyphicon-upload"></span>Upload File</button>
@@ -109,7 +175,6 @@ class CoreIdentity extends React.Component {
 			recovery_id:[],
 			owner_token_id:[],
 			control_token_id:[],
-			files:[],
 			showModal: false
 		};
 		
@@ -128,44 +193,8 @@ class CoreIdentity extends React.Component {
 		}
 	}
 	
-	uploadFile(e){
-		var fileInput = $("input[name=documents]");
-		
-		if(fileInput[0].files.length == 0) return;
-		
-		var pubKey = $("input[name=user_pubkey]").val();
-		var fData = new FormData();
-		
-		fData.append("user_pubkey", pubKey);
-		 $.each(fileInput[0].files, function(key, value){
-			fData.append(key, value);
-		});
-		
-		
-		var uploadBut = $('input[name=upload-file]'),
-			formBut = $('button[name=submit-form]');
-		
-		$.ajax({
-			url: "http://localhost:5050/ipfs/upload",
-			type: 'POST',
-			data: fData,
-			cache: false,
-			processData: false,
-			contentType: false,
-			beforeSend: function(xhr){
-				uploadBut.button('loading');
-				formBut.button('loading');
-			},
-			success: function (response) {
-				console.log(response);
-				$("input[name=documents]").empty();
-			},
-			complete: function(xhr, textStatus) {
-				uploadBut.button('reset');
-				formBut.button('reset');
-			} 
-		});
-		e.preventDefault();
+	getFileDetails(hash, fileHash){
+		console.log("Hash==>"+hash+" ==> "+ fileHash);
 	}
 	
 	submitCoid(e){
@@ -211,9 +240,7 @@ class CoreIdentity extends React.Component {
 					<div className="form-group">
 						<label htmlFor="unique_id">Unique Id</label>
 						{this.state.inputs.map(input => <UniqIdForm handleShowModal={this.handleShowModal.bind(this)} min={this.state.subform_cont} max="10" key={input} />)}
-						
 					</div>
-					
 					<div className="form-group"> 
 						<div className="col-md-offset-6 col-md-6 "> 
 							<p></p>
@@ -247,23 +274,13 @@ class CoreIdentity extends React.Component {
 						<TagsInput {...inputAttrs} value={this.state.control_token_id} onChange={(e)=>{ this.onFieldChange("control_token_id", e) } } />
 					</div>
 					<div className="form-group">
-						<label htmlFor="documents" className="col-md-12">Documents</label>
-						<div className="col-md-4">
-							<input type="file" className="form-control" name="documents" multiple />
-							<input type="hidden" className="form-control" name="user_pubkey" value="1dc99871943ad3a715f022273513a393564f9b060c4c047920fc1425b90b7740" />
-						</div>
-						<div className="col-md-8">
-							<input type="button" data-loading-text="Uploading Files..." value="Upload" name="upload-file" className="btn btn-primary" onClick={this.uploadFile.bind(this)} />
-						</div>
-					</div>
-					<div className="form-group">
 					  <div className="col-sm-6">
 					  <br/>
 						<button className="btn btn-primary" data-loading-text="Submit Identity" name="submit-form" type="submit">Submit Identity</button>
 					  </div>
 					</div>
 				</form>
-				{this.state.showModal ? <UploadIpfsFile handleHideModal={this.handleHideModal}/> : null}
+				{this.state.showModal ? <UploadIpfsFile dataHandler={this.getFileDetails.bind(this)} handleHideModal={this.handleHideModal}/> : null}
         </div>
     );
    }

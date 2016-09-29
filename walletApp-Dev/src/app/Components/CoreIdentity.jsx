c652ef0766cb5bcc68da0eca3a9231ef023995c4
Arun Kumar <arunkumarsekar@hotmail.com>
2016-09-29 18:41:31 +0530

    form value encryption

--- walletApp-Dev/src/app/Components/CoreIdentity.jsx
+++ walletApp-Dev/src/app/Components/CoreIdentity.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import TagsInput from 'react-tagsinput';
+import { sha3_256 } from 'js-sha3';
+
+//TODO : Namespace validation 
 
 class UploadIpfsFile extends React.Component {
 
@@ -48,10 +51,9 @@ class UploadIpfsFile extends React.Component {
 		e.preventDefault();
 		if(this.state.selected != "0"){
 			var hash, fileHash;
-			[hash, fileHash] = this.state.selected.split("|");
-			this.props.dataHandler(hash, fileHash);
+			this.props.dataHandler(this.state.selected);
+			$("button.close").trigger("click");
 		} else {
-			console.log(this.state.files);
 			if(this.state.files.size > 0){
 				var fileInput = $("input[name=newdoc]");
 				var fData = new FormData();
@@ -67,9 +69,8 @@ class UploadIpfsFile extends React.Component {
 					},
 					success: function(resp){
 						if(resp.uploded && resp.uploded.length > 0){
-							var hash = resp.uploded[0].hash,
-								fileHash = resp.uploded[0].file_hash;
-							this.props.dataHandler(hash, fileHash);
+							var filedata = resp.uploded[0].hash+"|"+resp.uploded[0].file_hash;
+							this.props.dataHandler(filedata);
 							$("button.close").trigger("click");
 						}
 					}.bind(this),
@@ -149,10 +150,10 @@ class UniqIdForm extends React.Component {
 		return(
 			<div className="form-group col-md-12">
 				<div className="col-md-10">
-					<input name="uniq_id_key" className="form-control col-md-4" type="text" placeholder="Label" />
+					<input name={'label-'+this.props.labelref} className="form-control col-md-4" type="text" placeholder="Label"  />
 				</div>
 				<div className="col-md-2">
-					<button type="button" onClick={this.props.handleShowModal} className="btn btn-warning pull-right"><span className="glyphicon glyphicon-upload"></span>Upload File</button>
+					<button type="button" data-id={this.props.labelref} onClick={this.props.handleShowModal} className="btn btn-warning pull-right"><span className="glyphicon glyphicon-upload"></span>Upload File</button>
 				</div>
 			</div>
 		);
@@ -167,7 +168,10 @@ class CoreIdentity extends React.Component {
 		super(props);
 		this.state = {
 			name:'',
-			unique_id:{},
+			file_attrs:[],
+			pubKey: '1dc99871943ad3a715f022273513a393564f9b060c4c047920fc1425b90b7740',
+			message:'',
+			signature:'',
 			inputs: ['input-0','input-1'],
 			official_id:[],
 			owner_id:[],
@@ -175,7 +179,8 @@ class CoreIdentity extends React.Component {
 			recovery_id:[],
 			owner_token_id:[],
 			control_token_id:[],
-			showModal: false
+			showModal: false,
+			tmpFile:''
 		};
 		
 		this.maxUniqAttr = 10;
@@ -185,29 +190,141 @@ class CoreIdentity extends React.Component {
 	
 	onFieldChange(inputField, e){
 		var multipleValues = {};
-		if(inputField == "name"){
-			this.setState({name: e.target.value});
+		if(inputField == "name" || inputField == "signature" || inputField =="message"){
+			this.setState({[inputField]: e.target.value});
 		} else {
 			multipleValues[inputField] = e;
 			this.setState(multipleValues);
 		}
 	}
 	
-	getFileDetails(hash, fileHash){
-		console.log("Hash==>"+hash+" ==> "+ fileHash);
+	getHash(input){
+		var input = $.trim(input);
+		if(input){
+			var hash = sha3_256.update(input).hex();
+			return hash;
+		}
+		return input;
+	}
+	
+	getFileDetails(filedata){
+		var obj = {[this.state.tmpFile]:filedata};
+		this.setState({file_attrs: this.state.file_attrs.concat([obj])});
+	}
+	
+	getLabelValues(){
+		var labelVals = []
+		var _this = this;
+		$.each($("input[name^='label-']"), function(obj){
+			var value = $.trim($(this).val());
+			if(value.length > 0){
+				labelVals.push({
+					[$(this).attr('name').replace("label-","")] : value
+				});
+			}
+		});
+		return labelVals;
+	}
+	
+	prepareJsonToSubmit(){
+		var inputObj = {
+				"pubKey": this.state.pubKey,
+				"sig": this.state.signature,
+				"msg": this.state.message,
+				"name": this.state.name,
+				"uniqueId": this.createHashAttribute(this.state.file_attrs),
+				"uniqueIdAttributes": this.prepareUniqueIdAttrs(),
+				"ownershipId": this.createHashAttribute(this.state.owner_id),
+				"ownerIdList":this.valueIntoHash(this.state.owner_id),
+				"controlId": this.createHashAttribute(this.state.control_id),
+				"controlIdList": this.valueIntoHash(this.state.control_id),
+				"ownershipTokenId": this.createHashAttribute(this.state.owner_token_id),
+				"ownershipTokenAttributes":this.valueIntoHash(this.state.owner_token_id),
+				"ownershipTokenQuantity": 3,
+				"controlTokenId": this.createHashAttribute(this.state.control_token_id),
+				"controlTokenAttributes": this.valueIntoHash(this.state.control_token_id),
+				"controlTokenQuantity": 5,
+				"identityRecoveryIdList": [],
+				"recoveryCondition": 2,
+				"yesVotesRequiredToPass": 2 
+		};
+		return inputObj;
+	}
+	
+	createHashAttribute(values){
+		if($.isArray(values) && values.length > 0){
+			if($.isPlainObject(values[0])){
+				var str = "";
+				for(var i=0; i<values.length; i++){
+					for(var key in values[i]){
+						var hash, filehash;
+						[hash,filehash] = values[i][key].split("|");
+						if((values.length-1) == i)
+							str += hash;
+						else 
+							str += hash+"|";
+					}
+				}
+				return this.getHash(str);
+			} else {
+				var valStr = values.join("|");
+				return this.getHash(valStr);
+			}
+			
+		}
+		return '';
+	}
+	
+	valueIntoHash(values){
+		var newArr = [];
+		var _this = this;
+		if($.isArray(values)){
+			values.map((value) => {
+				newArr.push(_this.getHash(value));
+			});
+		};
+		return newArr;
+	}
+	
+	prepareUniqueIdAttrs(){
+		var newArr = [],
+			labels = this.getLabelValues();
+		for(var i=0; i<labels.length; i++){
+			var tmpArr = [];
+			for(var key in labels[i]){
+				tmpArr.push(labels[i][key]);
+				var ipfsHash, fileHash;
+				[ipfsHash, fileHash] = this.state.file_attrs[i][key].split("|");
+				tmpArr.push(fileHash);
+				tmpArr.push(ipfsHash);
+			}
+			newArr.push(tmpArr);
+		}
+		return newArr;
 	}
 	
 	submitCoid(e){
 		e.preventDefault();
-		console.log("submitted values ==> ", this.state);
+		var json = this.prepareJsonToSubmit();
+		$.ajax({
+			url: 'http://localhost:5050/gk/gatekeeper'
+			type: 'POST',
+			data: json,
+			success: function(res){
+				// do something
+			},
+			complete: function(){
+				// do something
+			}
+		});
 	}
 
 	handleHideModal(){
 		this.setState({showModal: false});
 	}
 
-	handleShowModal(){
-        this.setState({showModal: true});
+	handleShowModal(e){
+        this.setState({showModal: true, tmpFile: $(e.target).attr('data-id')});
     }
 
 	appendInput() {
@@ -239,7 +356,7 @@ class CoreIdentity extends React.Component {
 					</div>
 					<div className="form-group">
 						<label htmlFor="unique_id">Unique Id</label>
-						{this.state.inputs.map(input => <UniqIdForm handleShowModal={this.handleShowModal.bind(this)} min={this.state.subform_cont} max="10" key={input} />)}
+						{this.state.inputs.map(input => <UniqIdForm handleShowModal={this.handleShowModal.bind(this)} min={this.state.subform_cont} max="10" key={input} labelref={input} />)}
 					</div>
 					<div className="form-group"> 
 						<div className="col-md-offset-6 col-md-6 "> 
@@ -248,6 +365,10 @@ class CoreIdentity extends React.Component {
 								<span className="glyphicon glyphicon-plus"></span>Add More
 							</button>
 						</div>
+					</div>
+					<div className="form-group">
+						<label htmlFor="signature">Signature</label>
+						<input className="form-control" id="signature" type="text" name="signature" onChange={ (e)=>{ this.onFieldChange("signature", e) } }/>
 					</div>
 					<div className="form-group">
 						<label htmlFor="official_id">Official ID</label>
@@ -274,8 +395,13 @@ class CoreIdentity extends React.Component {
 						<TagsInput {...inputAttrs} value={this.state.control_token_id} onChange={(e)=>{ this.onFieldChange("control_token_id", e) } } />
 					</div>
 					<div className="form-group">
+						<label htmlFor="message">Message</label>
+						<input className="form-control" id="message" type="text" name="message" onChange={ (e)=>{ this.onFieldChange("message", e) } }/>
+					</div>
+					<div className="form-group">
 					  <div className="col-sm-6">
 					  <br/>
+						<input type="hidden" name="pubkey" value={this.state.pubKey} />
 						<button className="btn btn-primary" data-loading-text="Submit Identity" name="submit-form" type="submit">Submit Identity</button>
 					  </div>
 					</div>

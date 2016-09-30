08c5b432cd9e59c3d772c0333ae4efadd651d197
Arun Kumar <arunkumarsekar@hotmail.com>
2016-09-30 18:24:19 +0530

    Add files via upload

--- Notification/DigitalTwin/index.js
+++ Notification/DigitalTwin/index.js
@@ -3,6 +3,7 @@ var app = require('express')(),
     bodyParser = require('body-parser'),
 	fileUpload = require('express-fileupload'),
 	ballotCtrl = require('./ballotCtrl.js'),
+	expiredNotification = require('./expiredNotification.js'),
 	IPFS = require('./ipfs.js');
 
  // for parsing application/json
@@ -13,9 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(fileUpload());
 
-/**
- * 
- */
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");

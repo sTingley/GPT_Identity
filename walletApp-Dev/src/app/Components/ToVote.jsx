a7ed3d59d0ee6081abf7a7b14a04d35bd38fc29f
sTingley <smtingle@syr.edu>
2016-10-14 18:22:33 -0400

    Update ToVote.jsx
    added getCoidData method

--- walletApp-Dev/src/app/Components/ToVote.jsx
+++ walletApp-Dev/src/app/Components/ToVote.jsx
@@ -44,13 +44,50 @@ class Votes extends React.Component {
 		
 	}
 	
+	getCoidData(e){
+		e.preventDefault();
+		var ele = $(e.target);
+		var index = parseInt(ele.attr("data-index"));
+		
+		var json = {
+			"txnDesc": this.refs["desc-"+index].value,
+			"signature": '7051442bbf18bb2c86cbc8951a07e27ec6ba05ac3fa427e4c6b948e3dcf91a94046b048edf52445fb22cc776a94b87c3f55426f993458ec744f61f09fb46eeaa',
+			"publicKey": localStorage.getItem("pubKey"),			//need to change ballot app 'requesterVal'
+			"proposalID": this.refs["proposalid-"+index].value,
+			"vote": parseInt(ele.attr("data-val"))
+		};
+		
+		
+		$.ajax({
+			url: twinUrl + 'getCoidData',
+			type: 'POST',
+			data: json,
+			success: function(res){
+				if(res.status == "Ok" && res.msg == "true"){
+					//var proposalID = res.proposalID || pid;
+					$.ajax({
+						//url: twinUrl + proposalID + "/" + localStorage.getItem("pubKey"),
+						type: 'GET',
+						complete: function(xhr){
+						//	alert("vote successfully submitted");
+							window.location.reload();
+						}
+					});
+				} else {
+					alert('Unable to get coid data')
+				}
+			}
+		})
+	}
+	
 	render(){
 		return(
 			<tr>
 				<td>
 					<h3>{this.props.msg}</h3>
 					<textarea className="form-control" ref={"desc-"+this.props.index} placeholder="Comments here.." />
-					<input type="hidden" ref={"proposalid-"+this.props.index} value={this.props.prposalid} />
+					<input ref={"proposalid-"+this.props.index} value={this.props.prposalid} />
+					<button type="button" data-index={this.props.index} data-val="" onClick={this.getCoidData.bind(this)}  className="btn btn-primary btn-block">retrieve data</button>
 				</td>
 				<td>
 					<p>

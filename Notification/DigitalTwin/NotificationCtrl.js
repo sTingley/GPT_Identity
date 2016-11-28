1269e0e98894488dd180b90ae1ee814459584dd1
PathHomotopy <lalvanss@miamioh.edu>
2016-11-28 17:07:49 -0500

    Create NotificationCtrl.js

---
+++ Notification/DigitalTwin/NotificationCtrl.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var fs = require ('fs');
+var Crypto = require('./cryptoCtr.js'),
+        keccak_256 = require('js-sha3').keccak_256,
+        config = require('./config.json');
+
+
+
+
+var NotificationCtrl = 
+{
+    
+}
+module.exports = NotificationCtrl;

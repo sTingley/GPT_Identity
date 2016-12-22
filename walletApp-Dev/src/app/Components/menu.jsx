233a1a767e850b6b207b7b7c3027beadc8e52691
PathHomotopy <lalvanss@miamioh.edu>
2016-12-22 11:41:43 -0500

    Update menu.jsx

--- walletApp-Dev/src/app/Components/menu.jsx
+++ walletApp-Dev/src/app/Components/menu.jsx
@@ -21,6 +21,7 @@ class Menu extends React.Component {
 				{ prop.loggedin ?  <li><Link to='/assets' activeClassName="active">Assets</Link></li> : '' }
 				{ prop.loggedin ?  <li><Link to='/docs' activeClassName="active">My Documents</Link></li> : '' }
 				{ prop.loggedin ?  <li><Link to='/myGatekeeper' activeClassName="active">My Gatekeeper</Link></li> : '' }
+                { prop.loggedin ?  <li><Link to='/mycoreidentity' activeClassName="active">My Core Identity</Link></li> : '' }
 			  </ul>
 			</div>
 		);

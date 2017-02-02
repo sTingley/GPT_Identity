e683b6cc8dca8f6225e1d8da5a42ceb0219b531e
sTingley <smtingley1@gmail.com>
2017-02-02 17:48:38 -0500

    Update menu.jsx

--- walletApp-Dev/src/app/Components/menu.jsx
+++ walletApp-Dev/src/app/Components/menu.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router';
 class Menu extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
 			userLoggedIn: this.props.loggedin
 		}
 	}
-	render () {
+	render() {
 		var prop = this.props;
 		return (
-		  <div className="col-sm-3 col-md-2 sidebar">
-			  <ul className="nav nav-pills nav-stacked">
-				<li><Link to='/home' activeClassName="active">Home</Link></li>
-				<li><Link to='/upload' activeClassName="active">Upload Keys</Link></li>
-				{ prop.loggedin ?  <li><Link to='/tovote' activeClassName="active">Vote</Link></li> : ''}
-				{ prop.loggedin ?  <li><Link to='/notifications' activeClassName="active">Notifications</Link></li> : ''}
-				{ prop.loggedin ?  <li><Link to='/register' activeClassName="active">Name Register</Link></li> : ''}
-				{ prop.loggedin ?  <li><Link to='/identity' activeClassName="active">Create Core Identity</Link></li> : '' }
-				{ prop.loggedin ?  <li><Link to='/assets' activeClassName="active">Assets</Link></li> : '' }
-				{ prop.loggedin ?  <li><Link to='/docs' activeClassName="active">My Documents</Link></li> : '' }
-				{ prop.loggedin ?  <li><Link to='/myGatekeeper' activeClassName="active">My Gatekeeper</Link></li> : '' }
-				{ prop.loggedin ?  <li><Link to='/mycoreidentity' activeClassName="active">COID Utility</Link></li> : '' }
-			  </ul>
+			<div className="col-sm-3 col-md-2 sidebar">
+				<ul className="nav nav-pills nav-stacked">
+					<li><Link to='/home' activeClassName="active">Home</Link></li>
+					<li><Link to='/upload' activeClassName="active">Upload Keys</Link></li>
+					{prop.loggedin ? <li><Link to='/tovote' activeClassName="active">Vote</Link></li> : ''}
+					{prop.loggedin ? <li><Link to='/notifications' activeClassName="active">Notifications</Link></li> : ''}
+					{prop.loggedin ? <li><Link to='/register' activeClassName="active">Name Register</Link></li> : ''}
+					{prop.loggedin ? <li><Link to='/identity' activeClassName="active">Create Core Identity</Link></li> : ''}
+					{prop.loggedin ? <li><Link to='/assets' activeClassName="active">Assets</Link></li> : ''}
+					{prop.loggedin ? <li><Link to='/docs' activeClassName="active">My Documents</Link></li> : ''}
+					{prop.loggedin ? <li><Link to='/myGatekeeper' activeClassName="active">My Gatekeeper</Link></li> : ''}
+					{prop.loggedin ? <li><Link to='/mycoreidentity' activeClassName="active">COID Utility</Link></li> : ''}
+				</ul>
 			</div>
 		);
 	}

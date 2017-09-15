6eef624d9352802acd7cf12c9a317124f1663456
sTingley <smtingle@syr.edu>
2017-09-15 13:45:55 -0400

    update bigchain oraclizer contract and js file

--- Oraclizers/BigchainOraclizer/ErisBigchainRequester.sol
+++ Oraclizers/BigchainOraclizer/ErisBigchainRequester.sol
@@ -1,180 +1,148 @@
-contract ErisBigchainRequester
-{
+contract ErisBigchainRequester {
     //this is analagous to "chairperson" in "Ballot"
     //this makes sure only the application can call functions,
     //which requesters should not be able to call:
     //this will be hardcoded in this contract
-    address chairperson;
+    address chairperson;//will need to be a list of accounts later on
 
-    event CallbackReady(address addr);
-    event requestMade(address addr);
+    event CallbackReady(bytes32 addr);
+    event requestMade(bytes32 addr);
 
     //Create the structue for requests
     //it will prove useful in the future
-    struct requestStruct
-    {
+    struct requestStruct {
         uint txnInProgress;
         string theRequest;
     }
 
     //Mappings (could have put callbacks in requestStruct?)
-    mapping (address => requestStruct) requests;
-    mapping (address => string) callbacks;
+    mapping (bytes32 => requestStruct) requests;
+    mapping (bytes32 => string) callbacks;
 
     //This information is needed to implement first-in-first-out
-    address[] indexer;
+    bytes32[] indexer;
 
-    function ErisBigchainRequester()
-    {
+    modifier onlyBy(address _account) {
+        if (_account != chairperson) throw;
+        _
+    }
+
+    //check there is no previous request with calling account
+    modifier txnInProgressCheck(bytes32 _account) {
+        if (requests[_account].txnInProgress != 0) throw;
+        _
+    }
+
+    function ErisBigchainRequester() {
         chairperson = msg.sender;
     }
 
-
-    function BigChainQuery(string requestInfo)
-    {
-
-        //check there is no previous request with msg.sender
-        if(requests[msg.sender].txnInProgress != 0)
-        {
-            throw;
-        }
+    function BigChainQuery(string requestInfo, bytes32 requester)
+    txnInProgressCheck(requester) {
 
         //no previous request, add to the mapping of requests
-        requests[msg.sender].txnInProgress = 1;
-        requests[msg.sender].theRequest = requestInfo;
+        requests[requester].txnInProgress = 1;
+        requests[requester].theRequest = requestInfo;
 
         //create the callback!
-        callbacks[msg.sender] = "Your transaction is in progress.";
+        callbacks[requester] = "Your transaction is in progress.";
 
         //push into the indexer
-        indexer.push(msg.sender);
+        indexer.push(requester);
 
-        requestMade(msg.sender);
-
+            requestMade(requester);
     }
 
-
-    function removeMyRequest()
+    function removeMyRequest(bytes32 requester)
     {
-
         //(1) FIND index of msg.sender (there can be only one):
         uint theIndex = 0;
                 bool check = false;
 
-        for (uint i = 0; i < indexer.length; i++)
-        {
-            if(indexer[i] == msg.sender)
-            {
+        for (uint i = 0; i < indexer.length; i++) {
+            if (indexer[i] == requester) {
                 theIndex = i;
                 check = true;
             }
         }
 
-        if(check)
-        {
-        //(2) Remove it, without leaving a gap, so we can treat it as a stack:
-        //does doing this imply the need of mutex logic (confused multiple deletes)???
-        for (uint j = 0; j < indexer.length; j++)
-        {
-            if( j >= theIndex && j < indexer.length -1)
-            {
-                indexer[j] = indexer[j+1];
+        if (check) {
+            //(2) Remove it, without leaving a gap, so we can treat it as a stack:
+            //does doing this imply the need of mutex logic (confused multiple deletes)???
+            for (uint j = 0; j < indexer.length; j++) {
+                if (j >= theIndex && j < indexer.length -1) {
+                    indexer[j] = indexer[j+1];
+                }
             }
+
+            delete(indexer[indexer.length - 1]);
         }
 
-        delete(indexer[indexer.length - 1]);
-        }
+        //address addr = msg.sender;
 
-
-        address addr = msg.sender;
-
-
-        callbacks[addr] = "";
-        requests[addr].txnInProgress = 0;
-        requests[addr].theRequest = "";
+        callbacks[requester] = "";
+        requests[requester].txnInProgress = 0;
+        requests[requester].theRequest = "";
 
     }
 
-
-    function myCallback() returns (string userCallback)
-    {
-        userCallback = callbacks[msg.sender];
+    function myCallback(bytes32 requester) returns (string userCallback) {
+        userCallback = callbacks[requester];
     }
-
 
     //this function is intended for requesterApp.js
     //check if the list is empty
-    function listIsEmpty() returns (bool emptyList)
-    {
-        if(msg.sender == chairperson)
-        {
-            emptyList = true;
-            if(indexer.length == 0)
-            {
-                emptyList = true;
-            }
+    function listIsEmpty() onlyBy(msg.sender) returns (bool emptyList) {
 
-            for (uint n = 0; n < indexer.length; n++)
-            {
-                address curAddr = indexer[n];
-                if(requests[curAddr].txnInProgress == 1)
-                {
-                        emptyList = false;
-                }
+        emptyList = true;
+        if (indexer.length == 0) {
+            emptyList = true;
+        }
+
+        for (uint n = 0; n < indexer.length; n++) {
+            bytes32 curAddr = indexer[n];
+            if (requests[curAddr].txnInProgress == 1) {
+                emptyList = false;
             }
         }
     }
-
 
     //this function is intended for requesterApp.js
     //0 is the index of the current address in the stack
-    function getCurrentInList() returns (address addr)
-    {
-        if(msg.sender == chairperson)
-        {
-            bool checkIt = false;
+    function getCurrentInList() onlyBy(msg.sender) returns (bytes32 addr) {
 
+            bool checkIt = false;
             uint theIndex = 0;
-            for (uint k = 0; k < indexer.length; k++)
-            {
-                address current = indexer[k];
-                if(requests[current].txnInProgress == 1)
-                {
-                        if(checkIt == false)
-                        {
-                                theIndex = k;
-                                checkIt = true;
-                        }
+
+            for (uint k = 0; k < indexer.length; k++) {
+            bytes32 current = indexer[k];
+
+            if (requests[current].txnInProgress == 1) {
+                if (checkIt == false){
+                    theIndex = k;
+                    checkIt = true;
                 }
             }
-            if(checkIt == true)
-            {
+            }
+            if (checkIt == true) {
                 addr = indexer[theIndex];
             }
-        }
-    }
 
+    }
 
     //this function is intended for requesterApp.js
     //this passes the request by address
-    function getRequestByAddress(address addr) returns (string request)
-    {
-        if(msg.sender == chairperson)
-        {
-            return requests[addr].theRequest;
-        }
-    }
+    function getRequestByPubKey(bytes32 pubKey) onlyBy(msg.sender) returns (string request) {
 
+        return requests[pubKey].theRequest;
+    }
 
     //allows the javascript application to set values
-    function setCurrentInList(address addr, string response)
-    {
-        if(msg.sender == chairperson)
-        {
-            CallbackReady(msg.sender);
-            requests[addr].txnInProgress = 0;
-            callbacks[addr] = response;
+    function setCurrentInList(bytes32 addr, string response) {
 
-        }
+        CallbackReady(addr);
+        requests[addr].txnInProgress = 0;
+        callbacks[addr] = response;
     }
 }
+
--- Oraclizers/BigchainOraclizer/ErisBigchainService.js
+++ Oraclizers/BigchainOraclizer/ErisBigchainService.js
@@ -5,6 +5,31 @@
 var chainConfig = require('/home/demoadmin/.eris/ErisChainConfig.json')
 console.log("Chain Configuration Account: " + chainConfig.primaryAccount)
 console.log("Chain URL: " + chainConfig.chainURL)
+var chain = 'primaryAccount';
+var ErisAddress = chainConfig[chain].address;
+console.log("\n\n\nChain Address: " + ErisAddress);
+//this library is needed to calculate hash of blockchain id (chain name) and bigchain response
+var keccak_256 = require('js-sha3').keccak_256;
+console.log("\n\n\nChain Address Hash: " + keccak_256(ErisAddress));
+
+
+//port listening libraries
+var express = require('express')
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.set('trust proxy', true);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, application/json-rpc");
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(morgan('dev'));
 
 
 //for calling contract:
@@ -17,6 +42,7 @@ var accounts = require('./accounts.json')
 var chainUrl
 var manager
 var contract
+var waitingList = [];
 
 //this is for signature generation:
 var crypto = require('crypto')
@@ -54,6 +80,264 @@ function createSignature(nonHashedMessage, callback) {
 }
 
 
+function bigchainPost(OwnerPubKey, proposalID, coidData, blockNumber, blockHash, blockchainID, timestamp, validatorSigs, serviceSig, callback) {
+
+    var thePubkey = OwnerPubKey;
+    console.log("In function bigchainIt, pubKey of eris account (ownerPubKey) is: " + thePubkey)
+
+    var description = "Recovery";
+    var attrs;
+    if (typeof (coidData.uniqueIdAttributes) == 'string') {
+        attrs = coidData.uniqueIdAttributes.split(',');
+    }
+    else { attrs = coidData.uniqueIdAttributes }
+
+    let trimmed = []
+    //trimming IPFS hashes and sha3 hashes from the uniqueIdAttrs before we write to bcdb
+    for (var i = 0; i < attrs.length; i += 3) {
+        trimmed.push(attrs[i]);
+    }
+
+    coidData.coidAddr = "";
+    coidData.gatekeeperAddr = "";
+    coidData.uniqueIdAttributes = trimmed;
+
+    //NOTE: signatures inputted to this object should include msg hash, signature and public key
+    //NOTE: Coid_Data should include uniqueID and the signature of the one requesting a core identity
+    var bigchainInput = {
+        "Description": description,
+        "proposalID": proposalID,
+        "Coid_Data": coidData,
+        "blockNumber": blockNumber,
+        "blockHash": blockHash,
+        "blockchainID": blockchainID,
+        "blockchain_timestamp": timestamp,
+        "validator_signatures": validatorSigs,
+        "service_signature": serviceSig
+    };//end json struct
+
+    var metadata = { "bigchainID": arguments[10], "bigchainHash": arguments[11] } || "";
+    coidData.bigchainID = "";
+    coidData.bigchainHash = "";
+
+    bigchainInput = JSON.stringify({ "data": bigchainInput, "metadata": metadata })
+    console.log("In function bigchainIt, the input to be sent to bigchain is: " + bigchainInput)
+    var bigchainEndpoint = 'addData/' + thePubkey + '/1'
+    var theobj = { "method": "POST", "stringJsonData": bigchainInput, "endpoint": bigchainEndpoint }
+    console.log("Bigchain Request: " + JSON.stringify(theobj))
+
+    contract.BigChainQuery(JSON.stringify(theobj), keccak_256(coidData.pubKey).toUpperCase(), function (error, result) {
+
+        console.log("A million stars ***************************************************************************************")
+        var theEvent;
+        contract.CallbackReady(function (error, result) {
+            theEvent = result;
+        },
+            function (error, result) {
+                console.log("addr: " + result.args.addr + "\nkey: " + keccak_256(coidData.pubKey).toUpperCase())
+                if (keccak_256(coidData.pubKey).toUpperCase() == result.args.addr) {
+
+                    contract.myCallback(keccak_256(coidData.pubKey).toUpperCase(), function (error, result) {
+
+                        console.log("RESULT: " + result);
+                        var bigchainID = JSON.parse(result).response;
+                        console.log("Result.response: " + bigchainID)
+                        bigchainID = JSON.parse(bigchainID).id;
+                        var bigchainHash = keccak_256(JSON.parse(result).response);
+                        console.log("************: " + JSON.parse(result).response);
+
+                        var signature = JSON.parse(result).signature
+                        var msg = JSON.parse(result).msg
+                        var pubKey = JSON.parse(result).pubKey
+                        console.log("pubkey returns is ......: " + pubKey)
+
+                        //verify oraclizer signature
+                        var logme = ed25519.Verify(new Buffer(msg), new Buffer(signature, "hex"), new Buffer(pubKey, "hex"))
+                        console.log(logme)
+
+                        //for debugging--ignore:
+                        if (logme == true) {
+                            console.log("logme is the bool true");
+                        }
+                        else {
+                            console.log("logme is not bool true but if this says true, it is a string: " + logme)
+                        }
+
+                        callback(result, bigchainID, bigchainHash)
+
+                        //stop event listening
+                        theEvent.stop();
+
+                    })//end calling of myCallback
+
+                }//end if statement
+
+            })//end callback listening
+
+
+    })//end bigchain query
+
+}// end bigchain Post
+
+function bigchainGet(pubKey, txid, callback) {
+    var thePubkey = ErisAddress;
+    var bigchainEndpoint = 'getTransaction/' + txid;
+    var theobj = { "method": "GET", "stringJsonData": "", "endpoint": bigchainEndpoint }
+    console.log("Bigchain Request: " + JSON.stringify(theobj))
+
+    contract.BigChainQuery(JSON.stringify(theobj), keccak_256(pubKey).toUpperCase(), function (error, result) {
+
+        var theEvent;
+        contract.CallbackReady(function (error, result) {
+            theEvent = result;
+        },
+            function (error, result) {
+
+                if (keccak_256(pubKey).toUpperCase() == result.args.addr) {
+                    console.log("callbackready: " + JSON.stringify(result))
+
+                    contract.myCallback(keccak_256(pubKey).toUpperCase(), function (error, result) {
+
+                        console.log("RESULT: " + result);
+                        var bigchainID = JSON.parse(result).response;
+                        console.log("\nResult.response: " + bigchainID)
+                        bigchainID = JSON.parse(bigchainID).id;
+                        var bigchainHash = keccak_256(JSON.parse(result).response);
+
+                        var signature = JSON.parse(result).signature
+                        var msg = JSON.parse(result).msg
+                        var pubKey = JSON.parse(result).pubKey
+                        console.log("pubkey returns is ......: " + pubKey)
+
+                        //verify oraclizer signature
+                        var logme = ed25519.Verify(new Buffer(msg), new Buffer(signature, "hex"), new Buffer(pubKey, "hex"))
+                        console.log(logme)
+
+                        //for debugging--ignore:
+                        if (logme == true) {
+                            console.log("logme is the bool true");
+                        }
+                        else {
+                            console.log("logme is not bool true but if this says true, it is a string: " + logme)
+                        }
+
+                        callback(result, bigchainID, bigchainHash)
+
+                        //stop event listening
+                        theEvent.stop();
+
+                    })//end calling of myCallback
+
+                }//end if statement
+
+            })//end callback listening
+
+    })//end bigchain query
+}//end bigchain get
+
+
+function bigchainTransfer(toPubKey, fromPubKey, txid, flag, callback) {
+    //var thePubkey = ErisAddress;
+    var bigchainEndpoint = 'transfer';
+    var bigchainInput = {
+        "toPubKey": toPubKey,
+        "fromPubKey": fromPubKey,
+        "txid": txid,
+        "flag": flag
+    }
+    var theobj = { "method": "POST", "stringJsonData": JSON.stringify(bigchainInput), "endpoint": bigchainEndpoint }
+    console.log("Bigchain Request: " + JSON.stringify(theobj))
+
+    contract.BigChainQuery(JSON.stringify(theobj), keccak_256(fromPubKey).toUpperCase(), function (error, result) {
+        console.log("bigchain query called");
+        var theEvent;
+        contract.CallbackReady(function (error, result) {
+            theEvent = result;
+        },
+            function (error, result) {
+                console.log("callback ready called");
+                if (keccak_256(fromPubKey).toUpperCase() == result.args.addr) {
+                    console.log("callbackready: " + JSON.stringify(result))
+
+                    contract.myCallback(keccak_256(fromPubKey).toUpperCase(), function (error, result) {
+
+                        console.log("RESULT: " + result);
+                        var bigchainID = JSON.parse(result).response;
+                        console.log("\nResult.response: " + bigchainID)
+                        bigchainID = JSON.parse(bigchainID).id;
+                        var bigchainHash = keccak_256(JSON.parse(result).response);
+
+                        var signature = JSON.parse(result).signature
+                        var msg = JSON.parse(result).msg
+                        var pubKey = JSON.parse(result).pubKey
+                        console.log("pubkey returns is ......: " + pubKey)
+
+                        //verify oraclizer signature
+                        var logme = ed25519.Verify(new Buffer(msg), new Buffer(signature, "hex"), new Buffer(pubKey, "hex"))
+                        console.log(logme)
+
+                        //for debugging--ignore:
+                        if (logme == true) {
+                            console.log("logme is the bool true");
+                        }
+                        else {
+                            console.log("logme is not bool true but if this says true, it is a string: " + logme)
+                        }
+
+                        callback(result, bigchainID, bigchainHash)
+
+                        //stop event listening
+                        theEvent.stop();
+
+                    })//end calling of myCallback
+
+                }//end if statement
+
+            })//end callback listening
+
+    })//end bigchain query
+}//end bigchain transfer
+
+function bigchainTransferFile(toPubKey, fromPubKey, callback) {
+    //var thePubkey = ErisAddress;
+    var bigchainEndpoint = 'transferFile';
+    var bigchainInput = {
+        "toPubKey": toPubKey,
+        "fromPubKey": fromPubKey
+    }
+    var theobj = { "method": "POST", "stringJsonData": JSON.stringify(bigchainInput), "endpoint": bigchainEndpoint }
+    console.log("Bigchain Request: " + JSON.stringify(theobj))
+
+    contract.BigChainQuery(JSON.stringify(theobj), keccak_256(fromPubKey).toUpperCase(), function (error, result) {
+        console.log("bigchain query called");
+        var theEvent;
+        contract.CallbackReady(function (error, result) {
+            theEvent = result;
+        },
+            function (error, result) {
+                console.log("callback ready called");
+                if (keccak_256(fromPubKey).toUpperCase() == result.args.addr) {
+                    console.log("callbackready: " + JSON.stringify(result))
+
+                    contract.myCallback(keccak_256(fromPubKey).toUpperCase(), function (error, result) {
+
+                        console.log("RESULT: " + result);
+
+                        callback(JSON.parse(result).response)
+
+                        //stop event listening
+                        theEvent.stop();
+
+                    })//end calling of myCallback
+
+                }//end if statement
+
+            })//end callback listening
+
+    })//end bigchain query
+}//end bigchain transferFile
+
+
 //this service never stops
 var queryAddr;
 var currentQuery;
@@ -72,12 +356,17 @@ contract.requestMade(
     },
     function (error, result) {
         //check if in progress
-        if (inProgress == 0) {
+        var shouldProcess = waitingList.indexOf(result.args.addr);
+        console.log("should process: " + shouldProcess);
+        console.log("result: " + JSON.stringify(result.args.addr));
+        console.log("wait list: " + waitingList);
+        if (inProgress == 0 && shouldProcess >= 0) {
             setTimeout(function () {
                 Process();
             }, 100)
         }
-    })
+    }
+)
 
 
 //continuous listening for CallbackReady event
@@ -87,12 +376,15 @@ contract.CallbackReady(
     },
     function (error, result) {
         //check if in progress
-        if (inProgress == 0) {
+        var shouldProcess = waitingList.indexOf(result.args.addr);
+        console.log("should process callbackready: " + shouldProcess);
+        if (inProgress == 0 && shouldProcess >= 0) {
             setTimeout(function () {
                 Process();
             }, 100)
         }
-    })
+    }
+)
 
 
 function Process() {
@@ -113,7 +405,7 @@ function Process() {
                 queryAddr = result;
                 console.log(queryAddr + " is current in list")
 
-                contract.getRequestByAddress(queryAddr, function (error, result) {
+                contract.getRequestByPubKey(queryAddr, function (error, result) {
 
                     currentQuery = result;
                     console.log(result + " is request by address");
@@ -166,6 +458,7 @@ function Process() {
                         });
                     }
                     else {
+                        console.log("POST URL: " + bigchainServer + endpoint);
                         request({
                             method: 'POST',
                             url: bigchainServer + endpoint,
@@ -207,4 +500,63 @@ function Process() {
 
 }//end recursive function
 
+// the pubkey stored in the waiting list is hashed
+//change name to postRequest later
+app.post("/preRequest", function (req, res) {
+    console.log("req: " + req.body);
+    var formdata = req.body;
+    waitingList.push(keccak_256(formdata.pubKey).toUpperCase());
+    bigchainPost(formdata.pubKey, formdata.proposalID, JSON.parse(formdata.data), formdata.blockNumber, formdata.blockHash, formdata.blockchainID, formdata.timestamp, formdata.validatorSigs, formdata.serviceSig, function (result, theId, theHash) {
+        var ret = {
+            theId: theId,
+            theHash: theHash
+        }
+        res.send(ret);
+    }, formdata.bigchainID, formdata.bigchainHash)
+    // console.log(result); 
+
+})
+
+app.post("/getRequest", function (req, res) {
+    console.log("GET req: " + req.body);
+    var formdata = req.body;
+    waitingList.push(keccak_256(formdata.pubKey).toUpperCase());
+    bigchainGet(formdata.pubKey, formdata.txid, function (getResult, getId, getHash) {
+        var ret = {
+            'getResult': getResult,
+            'getId': getId,
+            'getHash': getHash
+        }
+        console.log("sending file:\n" + JSON.stringify(ret));
+        res.send(ret);
+    })
+})
+
+app.post("/transferRequest", function (req, res) {
+    console.log("req: " + req.body);
+    var formdata = req.body;
+    waitingList.push(keccak_256(formdata.fromPubKey).toUpperCase());
+    bigchainTransfer(formdata.toPubKey, formdata.fromPubKey, formdata.txid, formdata.flag, function (getResult, getId, getHash) {
+        var ret = {
+            'transferResult': getResult,
+            'transferId': getId,
+            'transferHash': getHash
+        }
+        res.send(ret);
+    })
+})
+
+app.post("/transferFileRequest", function (req, res) {
+    console.log("req: " + req.body);
+    var formdata = req.body;
+    waitingList.push(keccak_256(formdata.fromPubKey).toUpperCase());
+    bigchainTransferFile(formdata.toPubKey, formdata.fromPubKey, function (result) {
+        res.send(result);
+    })
+})
+
+app.listen(3018, function () {
+    console.log("Connected to contract http://10.101.114.231:1337/rpc");
+    console.log("Listening on port 3018");
+})
 
